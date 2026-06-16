1285883772a2ebfa4cb833809a4577b5551001bd
nidhijangid-2120 <nidhijangid2120@gmail.com>
2026-06-16 10:06:50 +0530

    Floor Division File Added

---
+++ floorDivision.py
@@ -0,0 +1,10 @@
+# floor division
+a = 10
+b = 3
+c = a // b # means how many times b can fit into a without going over
+print(c)
+# floor division with negative numbers
+d = -10
+e = 3
+f = d // e
+print(f)
